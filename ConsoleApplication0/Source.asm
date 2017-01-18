598bf12425de4335fe1ae6bb9c422e0cadeaf8a8
Coderx64 <SNSmirnov1024@yandex.ru>
2017-01-18 18:23:15 +0400

    x64

--- ConsoleApplication0/Source.asm
+++ ConsoleApplication0/Source.asm
@@ -1,19 +1,17 @@
-.386
- 
-.model flat,stdcall
-includelib kernel32.lib
-
-ExitProcess PROTO		:DWORD
-MessageBoxA PROTO		:DWORD, :DWORD, :DWORD, :DWORD
-
-
+MessageBoxA PROTO :DWORD,:DWORD,:DWORD,:DWORD
+ExitProcess PROTO :DWORD
 .data
-
-sText db 'Hello world!',0
- 
+MsgCaption      db 'Win64 Iczelion''s lesson #2: MessageBox',0
+MsgBoxText      db 'Win64 Assembly is Great!',0
 .code
- 
-Start:
-	invoke MessageBoxA, 0, offset sText, 0, 0
-	invoke ExitProcess, 0
-end Start
+Start proc 
+    sub rsp,5*8
+    xor ecx,ecx;NULL
+    mov r9,rcx;MB_OK
+    lea rdx, MsgBoxText
+    lea r8, MsgCaption
+        call MessageBoxA
+        xor ecx,ecx
+    call ExitProcess
+Start endp
+end
